0e965d84305b1d7b38d504c9538ead2e4a0a6a4f
Mustafa TURAN <mustafaturan.net@gmail.com>
2017-03-23 21:50:09 +0000

    Add missing tests

---
+++ test/authable/authentications/error_test.exs
@@ -0,0 +1,26 @@
+defmodule Authable.Authentication.ErrorTest do
+  use ExUnit.Case
+  alias Authable.Authentication.Error
+
+  test "invalid_request" do
+    msg = "some"
+    expected = {:error, %{invalid_request: msg}, :bad_request}
+
+    assert expected == Error.invalid_request(msg)
+  end
+
+  test "invalid_token" do
+    msg = "some"
+    expected = {:error, %{invalid_token: msg}, :unauthorized}
+
+    assert expected == Error.invalid_token(msg)
+  end
+
+  test "insufficient_scope" do
+    required_scopes = ~w(read write)
+    expected = {:error, %{insufficient_scope:
+      "Required scopes are read, write."}, :forbidden}
+
+    assert expected == Error.insufficient_scope(required_scopes)
+  end
+end
