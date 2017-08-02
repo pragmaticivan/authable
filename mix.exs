0e36f4d916690b41e66b007d371065acedb539bc
Mustafa Turan <mustafaturan.net@gmail.com>
2017-08-02 02:31:46 +0100

    Update dependencies Add elixir 1.5 to travis

--- mix.exs
+++ mix.exs
@@ -37,17 +37,17 @@ defmodule Authable.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:postgrex, "~> 0.13"},
+      {:postgrex, "~> 0.13.3"},
       {:ecto, "~> 2.1"},
       {:comeonin, "~> 3.0"},
       {:secure_random, "~> 0.5.1"},
       {:plug, "~> 1.0 or ~> 1.1 or ~> 1.2 or ~> 1.3 or ~> 1.4"},
       {:poison, "~> 2.0 or ~> 2.1 or ~> 2.2 or ~> 3.0 or ~> 3.1"},
-      {:ex_machina, "~> 1.0.2", only: :test},
+      {:ex_machina, "~> 2.0", only: :test},
       {:credo, "~> 0.8.4", only: [:dev, :test]},
-      {:ex_doc, "~> 0.14", only: :dev},
-      {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
-      {:excoveralls, "~> 0.7", only: :test}
+      {:ex_doc, "~> 0.16.2", only: :dev},
+      {:dialyxir, "~> 0.5.1", only: [:dev], runtime: false},
+      {:excoveralls, "~> 0.7.2", only: :test}
     ]
   end
 
